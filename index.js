b2acbb7bc2d8556248180741f1b22716c52505ed
ghost-fire11 <164302905+ghost-fire11@users.noreply.github.com>
2024-04-08 06:52:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 //test
 var inter_time;
+var p_time = document.getElementById("id_time");
 inter_time = setInterval(function(){
   var data = new Date();
-  console.log(data);
+  // var year = data.getFullYear();
+  // var mouth = data.getMonth();
+  // var day = data.getDate();
+
+  p_time.innerText = data.toLocaleString();
+  console.log(data.toLocaleString());
+  console.log(p_time);
 }, 500);
